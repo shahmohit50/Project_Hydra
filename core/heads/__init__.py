790d939b92091714bbe9ae9542312e4dc5003213
Rachel <race2towin@gmail.com>
2025-10-04 21:50:38 +0530

    qwe

--- core/heads/__init__.py
+++ core/heads/__init__.py
@@ -2,7 +2,7 @@ from .story_weaver import StoryWeaver
 from .story_transformer import StoryTransformer
 from .code_forger import CodeForger
 from .comic_crafter import ComicCrafter
-from .voice_forge import VoiceForge
+# from .voice_forge import VoiceForge
 
 HEAD_REGISTRY = {
     "story_weaver": StoryWeaver,
